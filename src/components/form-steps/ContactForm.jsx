cabdc8db561f5abe90f845523c9fdb3a12596b63
894risper <zilper894@gmail.com>
2025-01-08 23:51:33 +0300

    added form validation in the contact form

--- src/components/form-steps/ContactForm.jsx
+++ src/components/form-steps/ContactForm.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react'
 import {useForm} from "react-hook-form"
-import { Input } from '../ui/inputs';
+
 const ContactForm = () => {
   const {register,handleSubmit,formState,reset}= useForm();
    
@@ -14,7 +14,7 @@ const handleFormSubmit=()=>{
 
 
   return (
-    <div className='border border-gray-100  w-full'>
+    <div className='border border-gray-100  w-full margin rounded-lg h-full'>
 
 <div>
   <h1>Contact Information</h1>
@@ -22,13 +22,13 @@ const handleFormSubmit=()=>{
 <form onSubmit={handleSubmit(handleFormSubmit)}>
   <div>
     <label htmlFor="fullname">FullName
-      <Input
+      <input
       type="text"
       placeholder='Fullname'
 {...register("fullname",{
   required:"your full name is required",
   pattern:{
-    value:/^[a-zA-Z]+$/,
+    value:/^[a-zA-Z ]+$/,
     message:"fullname should only contain letters"
   }
 })}
@@ -41,6 +41,56 @@ const handleFormSubmit=()=>{
 )}
     </label>
   </div>
+  <div>
+    <label htmlFor="email">Email
+<input type="text"
+placeholder='Email'
+{...register("email",{
+  required:"your email is required",
+  pattern:{
+    value:/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/,
+    message:"please enter a valid email"
+  }
+})}
+/>
+{formState.errors.email &&(
+  <p>
+    {formState.errors.email.message}
+  </p>
+)}
+
+    </label>
+
+  </div>
+  <div>
+    <label htmlFor="phoneNummber">Phone Number 
+<input
+ type="text" 
+placeholder='PhoneNumber'
+{...register("phoneNumber",{
+  required:"phone number required",
+  pattern:{
+    value:/^[0-9]$/,
+    message:"phone number should only contain numbers"
+  },
+  
+maxLength:{
+  value:"10",
+  message:"phone number should contain 10 numbers"
+}
+}
+
+)}
+/>
+{formState.errors.phoneNumber &&(
+  <p>
+    {formState.errors.phoneNumber.message}
+  </p>
+)}
+    </label>
+  </div>
+
+  <button onClick={handleSubmit(handleFormSubmit)}>Submit</button>
 </form>
 
 </div>
